5e7694ac835cdd2d24b50298c7f1823ed6eef9d0
axis133 <148345418+axis133@users.noreply.github.com>
2024-01-04 18:42:07 +0300

    Добавьте файлы проекта.

---
+++ ASDl5/ASDl5.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <queue>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+
+    vector<vector<int>> graph(N, vector<int>(M));
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> graph[i][j];
+        }
+    }
+
+    int minTime = INT_MAX;
+    int commandCenter = -1;
+
+    for (int i = 0; i < N; i++) {
+        vector<bool> visited(N, false);
+        queue<int> q;
+        q.push(i);
+        visited[i] = true;
+        int time = 0;
+
+        while (!q.empty()) {
+            int curr = q.front();
+            q.pop();
+            for (int j = 0; j < M; j++) {
+                if (graph[curr][j] == 1) {
+                    for (int k = 0; k < N; k++) {
+                        if (graph[k][j] == -1 && !visited[k]) {
+                            q.push(k);
+                            visited[k] = true;
+                        }
+                    }
+                }
+            }
+            time++;
+        }
+
+        if (time < minTime) {
+            minTime = time;
+            commandCenter = i;
+        }
+    }
+
+    cout << "Командный центр: " << commandCenter << endl;
+
+    return 0;
+}
